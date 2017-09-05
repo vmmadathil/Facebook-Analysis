e1125df5b2ea62496bdee420033dc384d46e65c6
Anshu Rao <Anshu@cometnet-10-21-16-66.utdallas.edu>
2017-09-05 15:21:38 -0500

    Fixed export of comments

--- FBscrape.py
+++ FBscrape.py
@@ -1,158 +1,67 @@
-import json
-import datetime
-import csv
-import time
-try:
-    from urllib.request import urlopen, Request
-except ImportError:
-    from urllib2 import urlopen, Request
+import json, datetime, requests
+import pandas as pd
 
-app_id = "<>"
-app_secret = "<>"
-file_id = "id"
+# Get FB API information
+with open("FBInfo.txt") as f:
+    APP_ID = f.readline().rstrip()
+    APP_SECRET = f.readline().rstrip()
 
-access_token = app_id + "|" + app_secret
+# Set Global Constants
+LIMIT = 100
+BASE_URL = "https://graph.facebook.com/v2.10/"
+COLUMNS = ["created_time", "message", "from", "id"]
 
+# Scrape a page for posts
+def scrapeFeedComments(data,page_id,access_token):
+    # Start scraping the page
+    print("{} : Scraping Posts From Page {} ...\n".format(datetime.datetime.now(),page_id))
 
+    # GET request
+    parameters = "?access_token=" + access_token + "&limit=" + str(LIMIT)
+    getPosts = requests.get(BASE_URL + page_id + "/posts" + parameters)
+    posts = getPosts.json()['data']
 
-def getFacebookCommentFeedUrl(base_url):
+    # For each post get the comments and subocomments
+    count = 0
+    for post in posts:
+        data  = scrapeCommentsFromPosts(data,post,access_token)
+    return data
 
-    # Construct the URL string
-    fields = "&fields=id,message,reactions.limit(0).summary(true)" + \
-        ",created_time,comments,from,attachment"
-    url = base_url + fields
-
-    return url
-
-
-
-def processComment(comment, status_id, parent_id=''):
-
-
-
-    comment_id = comment['id']
-    comment_message = '' if 'message' not in comment or comment['message'] \
-        is '' else unicode_decode(comment['message'])
-    comment_author = unicode_decode(comment['from']['name'])
-    num_reactions = 0 if 'reactions' not in comment else \
-        comment['reactions']['summary']['total_count']
-
-    if 'attachment' in comment:
-        attachment_type = comment['attachment']['type']
-        attachment_type = 'gif' if attachment_type == 'animated_image_share' \
-            else attachment_type
-        attach_tag = "[[{}]]".format(attachment_type.upper())
-        comment_message = attach_tag if comment_message is '' else \
-            comment_message + " " + attach_tag
-
-
-    comment_published = datetime.datetime.strptime(
-        comment['created_time'], '%Y-%m-%dT%H:%M:%S+0000')
-    comment_published = comment_published + datetime.timedelta(hours=-5)  # EST
-    comment_published = comment_published.strftime(
-        '%Y-%m-%d %H:%M:%S')
-
-
-    return (comment_id, status_id, parent_id, comment_message, comment_author,
-            comment_published, num_reactions)
-
-
-def scrapeFeedComments(page_id, access_token):
-    with open('{}_facebook_comments.csv'.format(file_id), 'w') as file:
-        w = csv.writer(file)
-        w.writerow(["comment_id", "status_id", "parent_id", "comment_message",
-                    "comment_author", "comment_published", "num_reactions",
-                    "num_likes", "num_loves", "num_wows", "num_hahas",
-                    "num_sads", "num_angrys", "num_special"])
-
-        num_processed = 0
-        scrape_starttime = datetime.datetime.now()
-        after = ''
-        base = "https://graph.facebook.com/v2.9"
-        parameters = "/?limit={}&access_token={}".format(
-            100, access_token)
-
-        print("Scraping {} Comments From Posts: {}\n".format(
-            file_id, scrape_starttime))
-
-        with open('{}_facebook_statuses.csv'.format(file_id), 'r') as csvfile:
-            reader = csv.DictReader(csvfile)
-
-
-            for status in reader:
-                has_next_page = True
-
-                while has_next_page:
-
-                    node = "/{}/comments".format(status['status_id'])
-                    after = '' if after is '' else "&after={}".format(after)
-                    base_url = base + node + parameters + after
-
-                    url = getFacebookCommentFeedUrl(base_url)
-                    # print(url)
-                    comments = json.loads(request_until_succeed(url))
-
-
-                    for comment in comments['data']:
-                        comment_data = processComment(
-                            comment, status['status_id'])
-
-
-
-
-                        if 'comments' in comment:
-                            has_next_subpage = True
-                            sub_after = ''
-
-                            while has_next_subpage:
-                                sub_node = "/{}/comments".format(comment['id'])
-                                sub_after = '' if sub_after is '' else "&after={}".format(
-                                    sub_after)
-                                sub_base_url = base + sub_node + parameters + sub_after
-
-
-
-
-
-                                    num_sub_special = sub_comment_data[
-                                        6] - sum(sub_reactions_data)
-
-                                    w.writerow(sub_comment_data +
-                                               sub_reactions_data + (num_sub_special,))
-
-                                    num_processed += 1
-                                    if num_processed % 100 == 0:
-                                        print("{} Comments Processed: {}".format(
-                                            num_processed,
-                                            datetime.datetime.now()))
-
-                                if 'paging' in sub_comments:
-                                    if 'next' in sub_comments['paging']:
-                                        sub_after = sub_comments[
-                                            'paging']['cursors']['after']
-                                    else:
-                                        has_next_subpage = False
-                                else:
-                                    has_next_subpage = False
-
-                        # output progress occasionally to make sure code is not
-                        # stalling
-                        num_processed += 1
-                        if num_processed % 100 == 0:
-                            print("{} Comments Processed: {}".format(
-                                num_processed, datetime.datetime.now()))
-
-                    if 'paging' in comments:
-                        if 'next' in comments['paging']:
-                            after = comments['paging']['cursors']['after']
-                        else:
-                            has_next_page = False
-                    else:
-                        has_next_page = False
-
-        print("\nDone!\n{} Comments Processed in {}".format(
-            num_processed, datetime.datetime.now() - scrape_starttime))
-
+def scrapeCommentsFromPosts(data,post,access_token):
+    # GET request
+    parameters = "?access_token=" + access_token + "&limit=" + str(LIMIT)
+    getComments = requests.get(BASE_URL + post['id'] + "/comments" + parameters)
+    comments = getComments.json()['data']
+    # Append all comments
+    dataToAdd = pd.DataFrame(comments,columns=COLUMNS)
+    data = data.append(dataToAdd,ignore_index=True)
+    return data
 
 if __name__ == '__main__':
-    scrapeFeedComments(file_id, access_token)
+    # Create the data frame
+    data = pd.DataFrame(columns=COLUMNS)
+    access_token = APP_ID + "|" + APP_SECRET
+
+    # What page do we want to run
+    page_id = "367963843082"
+
+    # Start the scraping
+    data = scrapeFeedComments(data,page_id, access_token)
+    listOfNames = []
+    listOfNameIDs = []
+    for entry in data['from']:
+        listOfNames.append(entry['name'])
+        listOfNameIDs.append(str(entry['id']))
+
+    data['User'] = pd.Series(listOfNames)
+    data['User_ID'] = pd.Series(listOfNameIDs,dtype=str)
+    del data['from']
+
+    # From here use awk to re-order columns or rename column names
+    # Use bash to sort or get unique names etc
+
+    # Export the table
+    print("Completed scraping. Exporting DataFrame...")
+    filename = page_id + "_facebook_comments.csv"
+    data.to_csv(filename, index=True, encoding='utf-8')
+    print("Exported!")
